1bb9618bf56dd05010a8e8d951525c7441545ba7
xys2002 <zcabxui@ucl.ac.uk>
2023-03-14 01:15:32 +0000

    del pycache

--- chatbotSite/webscraptool/env.py
+++
@@ -1,5 +0,0 @@
-MONGODB_URL = "mongodb://nhschatbotdb:fR3tHFqVSCFauKgWvMiigj3nNYi67yfXKvahPKekoUdXVT88HgPvlnqL4FY2875XLn6TsijMyXqgACDbeaftmQ==@nhschatbotdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@nhschatbotdb@"
-BING_AZURE_FUNC_URL = "https://bingaccessforwatson.azurewebsites.net/api/useQnA2ImproveAnswer?code=41mEZaj2kjJphYWmK6eHPFzQwQHLiVyW61QpEvrpGUcbAzFuAwDC3w=="
-BING_API_KEY = "ef99b91a0209431cb66dd4d32a0b20c6"
-IBM_NLU_URL = 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/2019387f-a518-46b0-aa72-ccc03cdf67c8'
-IBM_NLU_API_KEY = 'Iig5IogwUhp2dIo1FqAUEjkka7OoC42qXbWHWucIU9oU'
